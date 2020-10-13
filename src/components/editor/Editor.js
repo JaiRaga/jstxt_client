fdec810acf6ba5d835d0513832688035d2d93032
JaiRaga <raga.j.santhosh@gmail.com>
2020-10-13 12:25:14 +0530

    modified jss for text area and added support for scaling multiple editor

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import "../home/Home.css";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    gridColumn: "span 23",
+    gridRow: "span 24"
+  },
+  textarea: {
+    resize: "none",
+    gridColumn: "span 24",
+    gridRow: "span 24"
+  }
+}));
 
 const Editor = () => {
-  return <div>Editor</div>;
+  const classes = useStyles();
+  return (
+    <div className='codeeditor'>
+      <textarea className={classes.textarea} autofocus />
+    </div>
+  );
 };
 
 export default Editor;
