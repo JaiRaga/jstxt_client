f7c999552a1e2d7aaaac74f28bf34759713bef15
JaiRaga <raga.j.santhosh@gmail.com>
2020-10-22 15:53:29 +0530

    displays menu list on click

---
+++ src/components/layout/MenuList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuListItem from "./MenuListItem";
+
+const MenuList = () => {
+  return (
+    <div>
+      <MenuListItem />
+    </div>
+  );
+};
+
+export default MenuList;
